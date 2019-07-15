8d417fc341a86a05a801046bb7466dbba165ccb9
Taiga KIYOKAWA <taiga.k127@gmail.com>
2019-07-16 01:11:50 +0900

    yarn add react react-dom and create src/index.js

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Index extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>This is Root</h2>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Index />,
+  document.getElementById('root')
+);
