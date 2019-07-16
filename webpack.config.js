975d84068654526c8e43eaf8f27ed509abc5621a
Taiga KIYOKAWA <taiga.k127@gmail.com>
2019-07-16 22:39:37 +0900

    yarn add -D webpack-dev-server and edit webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,11 @@ const path = require('path');
 module.exports = {
   mode: 'production',
   entry: './src/index.js',
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    port: 3000,
+    compress: true,
+  },
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
