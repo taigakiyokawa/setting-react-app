9194a11f4bcffe765202adf76532f280c1f2b3da
Taiga KIYOKAWA <taiga.k127@gmail.com>
2019-07-16 15:30:09 +0900

    create .babelrc and set module rules in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,13 @@ module.exports = {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
 };
